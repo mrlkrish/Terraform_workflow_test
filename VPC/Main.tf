83181a617ede432ac7f0353bb3467be640de97b9
murali <mrlkrish@gmail.com>
2024-05-29 22:30:34 -0600

    added VPC

---
+++ VPC/Main.tf
@@ -0,0 +1,22 @@
+## backend data for terraform
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "remote" {
+  hostname = "app.terraform.io"  
+  organization = "Murali_Org"
+
+    workspaces {
+      name = "Terraform-CICD-VPC"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
+
