db04d370cf49fb88a0733334058262d2668ec7a5
murali <mrlkrish@gmail.com>
2024-05-29 23:03:34 -0600

    updated VPC

--- VPC/VPC.tf
+++ VPC/VPC.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "example_subnet" {
   //vpc_id                  = aws_vpc.example_vpc.id
   vpc_id                  = var.vpc_id
   cidr_block              = "10.0.1.0/24" // Set your desired subnet CIDR block
-  availability_zone       = "us-west-1"  // Set your desired availability zone
+  availability_zone       = "us-west-1a"  // Set your desired availability zone
   map_public_ip_on_launch = false          // Set to true if you want instances in this subnet to have public IP addresses
 
   tags = {
